258b5d0d2581abc32bf569394feab24654b18ff6
Sós Gellért <gellertsos@gmail.com>
2025-12-23 18:05:11 +0100

    feat: start implementing auth-service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,25 @@
-import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.log("Server Error: ", err);
 });
