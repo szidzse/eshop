424d357cb8eb7d70fb65efb3e1b1a41c858ba0ac
Sós Gellért <gellertsos@gmail.com>
2025-12-25 15:14:23 +0100

    feat: implement validateRegistration and checkOtpRestrictions and other helper functions

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,28 @@
+// Register a new user
+import { Request, Response, NextFunction } from "express";
+import {
+  validateRegistrationData,
+  checkOtpRestrictions,
+} from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  validateRegistrationData(req.body, "user");
+
+  const { name, email } = req.body;
+
+  const existingUser = await prisma.users.findUnique({
+    where: email,
+  });
+
+  if (existingUser) {
+    return next(new ValidationError("User already exists with this email!"));
+  }
+
+  await checkOtpRestrictions(email, next);
+};
---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,65 @@
+import crypto from "crypto";
+import { ValidationError } from "../../../../packages/error-handler";
+import redis from "../../../../packages/libs/redis";
+import { sendEmail } from "./sendMail";
+import { NextFunction } from "express";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const validateRegistrationData = (
+  data: any,
+  userType: "user" | "seller",
+) => {
+  const { name, email, password, phone_number, country } = data;
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    (userType === "seller" && (!phone_number || !country))
+  ) {
+    throw new ValidationError(`Missing required fields!`);
+  }
+
+  if (!emailRegex.test(email)) {
+    throw new ValidationError(`Invalid email format!`);
+  }
+};
+
+export const checkOtpRestrictions = async (
+  email: string,
+  next: NextFunction,
+) => {
+  if (await redis.get(`otp_lock:${email}`)) {
+    return next(
+      new ValidationError(
+        "Account locked due to multiple failed attempts! Try again after 30 minutes.",
+      ),
+    );
+  }
+
+  if (await redis.get(`otp_spam_lock:${email}`)) {
+    return next(
+      new ValidationError(
+        "Too many OTP requests! Please wait 1 hour before requesting again.",
+      ),
+    );
+  }
+
+  if (await redis.get(`otp_cooldown:${email}`)) {
+    return next(
+      new ValidationError("Please wait 1 minute before requesting a new OTP!"),
+    );
+  }
+};
+
+export const sendOtp = async (
+  name: string,
+  email: string,
+  template: string,
+) => {
+  const otp = crypto.randomInt(1000, 9999).toString();
+  await sendEmail(email, "Verify Your Email", template, { name, otp });
+  await redis.set(`otp:${email}`, otp, "EX", 300);
+  await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
+};
