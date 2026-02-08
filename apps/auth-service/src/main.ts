254ec6d4b1e3b644af8492732e931adb625d77c1
Sós Gellért <gellertsos@gmail.com>
2026-02-08 14:50:54 +0100

    chore: create a swagger docsumentation for auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 import cookieParser from "cookie-parser";
+import router from "./routes/auth.router";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument = require("./swagger-output.json");
 
 const app = express();
 
@@ -19,11 +22,21 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
 
+// Swagger Documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+  res.json(swaggerDocument);
+});
+
+// Routes
+app.use("/api", router);
+
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
   console.log(`Auth service is running at http://localhost:${port}/api`);
+  console.log(`Swagger Docs available at http://localhost:${port}/docs`);
 });
 
 server.on("error", (err) => {
